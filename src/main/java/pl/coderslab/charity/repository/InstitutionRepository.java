710b543a5ea8c35a892d35494ba0347b3a03e03b
proseb <orliks2290@gmail.com>
2020-12-22 10:01:15 +0100

    Viewing problems solved

--- src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
+++ src/main/java/pl/coderslab/charity/repository/InstitutionRepository.java
@@ -1,19 +1,14 @@
 package pl.coderslab.charity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import pl.coderslab.charity.entity.Institution;
 
 import java.util.List;
-
+@Repository
 public interface InstitutionRepository extends JpaRepository<Institution, Long> {
     //    Institution findOneByName(String institution);
 //    Institution findAllByNameLike(String institution);
-    List<Institution> findAll();
 
-    Institution findById(long id);
-
-    void delete(long id);
-
-    void save(Institution institution);
 }
-// findAll
